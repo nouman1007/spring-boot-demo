f2b9e736bd5a08d3fdd34554eb1bb63cdee1848f
nouman1007 <noumanshafiq84@gmail.com>
2022-08-20 20:39:17 +0500

    spring boot projects

---
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String demoString() {
+        return "Hello World";
+    }
+}
